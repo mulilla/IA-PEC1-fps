ed3ff73c5faeae76fe0dc9bf3094d0b8cfd3c4e5
mulilla <izar19mula97@gmail.com>
2020-03-29 17:16:47 +0200

    Activity 2 done

---
+++ Assets/Scripts/logicaActividad1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logicaActividad1 : MonoBehaviour
+{
+
+    private UnityEngine.AI.NavMeshAgent agent;
+    public bool isMoving = false;
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(camRay, out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/logicaActividad2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logicaActividad2 : MonoBehaviour
+{
+    
+    private UnityEngine.AI.NavMeshAgent agent;
+    public GameObject agentToFollow;
+
+    Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowAgent();
+    }
+
+    public void FollowAgent() {
+        //Pb
+        Vector3 targetPosition = agentToFollow.GetComponent<Transform>().position;
+        //Módulo del vector
+        Vector3 playerDistance = (targetPosition - (Vector3)transform.position);        
+        //Módulo velocidad deseado
+        Vector3 desiredVelocity = playerDistance.normalized * 4f;
+
+
+        Vector3 steering = desiredVelocity - velocity;
+
+
+        velocity += steering * Time.deltaTime;
+        
+        agent.destination += velocity*Time.deltaTime;
+        Debug.Log(agent.destination);
+        
+    }
+}
